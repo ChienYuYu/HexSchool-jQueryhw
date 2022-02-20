a6bfb7fb7094a881c8b6a3af54c8793aeec42869
陳建佑 <97875966+ChienYuYu@users.noreply.github.com>
2022-02-20 17:21:57 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-    $('.dropdown').click(function(event){
-        event.preventDefault();
-        $('.dropdown').toggleClass('active');
-        $('.d-menu').slideToggle();
-    });
+    
+    //原始寫的收合選單↓--------------------------------
+//     $('.dropdown').click(function(event){
+//         event.preventDefault();
+//         $('.dropdown').toggleClass('active');
+//         $('.d-menu').slideToggle();
+//     });
+    
+    
+    //助教建議的↓---------------------------
+//     $('.menu a').click(function(event){
+//     event.preventDefault();
+//     $('.dropdown').toggleClass('active');
+//     $('.d-menu').slideToggle();
+//     $(this).parent().siblings().find('.d-menu').slideUp();
+//     });
+    
+    //因為我menu多加了一個li，所以我又寫了這個版本↓-----------
+    $('.menu a').click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $(this).siblings('.d-menu').slideToggle();
+    $(this).parent().siblings().find('a').removeClass('active');
+    $(this).parent().siblings().find('.d-menu').slideUp();
+  });
+    
 
     //swiper---------------------------
     const swiper = new Swiper('.swiper', {
@@ -27,12 +48,23 @@ $(document).ready(function(){
       
       });
 
-    //.goTop-------------------------
+    //.goTop 向上捲-------------------------
     $('.goTop a').click(function(event){
         event.preventDefault();
         $('html,body').animate({scrollTop:0},800);
     });
-
+    
+     //gotop按鈕淡入淡出-------------------------
+  function showBtnCondition() {
+    if ($(this).scrollTop() > 300) {
+      $('.goTop a').fadeIn();
+    } else {
+      $('.goTop a').fadeOut();
+    }
+  }
+  $(window).scroll(showBtnCondition);
+    
+    //.goDown 向下捲-------------------------
     $('.goDown').click(function(event){
       event.preventDefault();
       $('html,body').animate({scrollTop:999},800);
